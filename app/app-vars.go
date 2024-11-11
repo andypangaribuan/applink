f0374314cd60e5c57a94a6e23485c70b8d1b80e3
Andy Pangaribuan <iam.pangaribuan@gmail.com>
2024-11-11 15:16:48 +0700

    add app variable

---
+++ app/app-vars.go
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2024.
+ * Created by Andy Pangaribuan. All Rights Reserved.
+ *
+ * This product is protected by copyright and distributed under
+ * licenses restricting copying, distribution and decompilation.
+ */
+
+package app
+
+var Env *stuEnv
